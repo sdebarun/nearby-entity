f3436b561b0d9eb7307abeec122a49b44c6f5216
Debarun Saha <sdebarun04@gmail.com>
2024-11-07 18:49:02 +0530

    tested with fields

--- src/Api.php
+++ src/Api.php
@@ -1,23 +1,24 @@
 <?php
+
 namespace Nearby;
+
 use GuzzleHttp\Client;
+
 class Api {
     protected Client $client;
-    public function __construct(private string $apiKey)
-    {
+    public function __construct(private string $apiKey) {
         $this->apiKey = $apiKey;
         $this->client = new Client();
-
     }
 
-    public function getNearbyPlaces(string $lat, string $lang, string| int $radius) : array {
+    public function getNearbyPlaces(string $lat, string $lang, string|int $radius, array $types, string $fields = 'places.id,places.displayName,places.formattedAddress'): array {
         $response = $this->client->post('https://places.googleapis.com/v1/places:searchNearby', [
             'query' => [
-                'fields' => '*',
+                'fields' => $fields,
                 'key' => $this->apiKey,
             ],
             'json' => [
-                "includedTypes" => ["police"],
+                "includedTypes" => $types,
                 "locationRestriction" => [
                     "circle" => [
                         "center" => [
@@ -37,7 +38,17 @@ class Api {
     }
 
 
-    public function getPlaceDetails(string $placeId){
-        
+    public function getPlaceDetails(string $placeId, string $fields = 'id,displayName,formattedAddress,nationalPhoneNumber,internationalPhoneNumber,googleMapsLinks') : array {
+        $response = $this->client->get("https://places.googleapis.com/v1/places/$placeId", [
+            'query' => [
+                'fields' => $fields,
+                'key'    => $this->apiKey,
+            ]
+        ]);
+
+        return [
+            'status' => $response->getStatusCode(),
+            'contents' => $response->getBody(),
+        ];
     }
 }
